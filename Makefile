4d78534d354ecd1f95c9ca06f76da4770c704221
Thomas Richter <thor@math.tu-berlin.de>
2017-01-31 11:44:34 +0100

    Added copyright notices.

--- Makefile
+++ Makefile
@@ -1,11 +1,28 @@
 #######################################################################
 ##
-## $Id: Makefile,v 1.19 2016/04/07 10:44:49 thor Exp $
+## $Id: Makefile,v 1.20 2017/01/31 10:44:08 thor Exp $
 ##
 #######################################################################
 ## Makefile for the difftest_ng project,
 ## THOR Software, 2. Jul. 2003, Thomas Richter for
 ## Accusoft
+##
+## This source file is part of difftest_ng, a universal image measuring
+## and conversion framework.
+##
+##  difftest_ng is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  difftest_ng is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with difftest_ng.  If not, see <http://www.gnu.org/licenses/>.
+##
 #######################################################################
 ##
 ## 
--- diff/debayer.cpp
+++ diff/debayer.cpp
@@ -1,9 +1,26 @@
 /*************************************************************************
-** Copyright (c) 2003-2010 Accusoft/Pegasus 				**
-** THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE				**
+** Copyright (c) 2003-2016 Accusoft 				        **
 **									**
-** Written by Thomas Richter (THOR Software) for Accusoft/Pegasus	**
+** Written by Thomas Richter (THOR Software) for Accusoft	        **
 ** All Rights Reserved							**
+**************************************************************************
+
+This source file is part of difftest_ng, a universal image measuring
+and conversion framework.
+
+    difftest_ng is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    difftest_ng is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with difftest_ng.  If not, see <http://www.gnu.org/licenses/>.
+
 *************************************************************************/
 
 /*
